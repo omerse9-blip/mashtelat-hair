3ce1b29875aa9beacb83d4b954e744ab2b5b78ba
omerse9-blip <omerse9@gmail.com>
2026-06-27 18:34:10 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 420, 640, 768, 1024, 1200],
+    imageSizes: [80, 120, 160, 240, 320, 480],
+  },
 };
 export default nextConfig;
