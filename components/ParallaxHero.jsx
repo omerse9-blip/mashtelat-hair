5b81119cf93cf1c380c4643330087c572ccbadcb
omerse9-blip <omerse9@gmail.com>
2026-08-20 22:13:26 +0300

    Update ParallaxHero.jsx

--- components/ParallaxHero.jsx
+++ components/ParallaxHero.jsx
@@ -122,7 +122,7 @@ export default function ParallaxHero({ children }) {
             padding: 23px 22px 20px;
             border-radius: 16px;
             gap: 9px;
-            background: rgba(247, 242, 233, 0.83);
+            background: rgba(247, 242, 233, 0.6);
           }
         }
       `}</style>
