939308d9559a2e3ee40e5775e96d363895c2ecba
omerse9-blip <omerse9@gmail.com>
2026-08-21 16:12:51 +0300

    Update NurseryCatalog.jsx

--- components/NurseryCatalog.jsx
+++ components/NurseryCatalog.jsx
@@ -29,11 +29,11 @@ export default function NurseryCatalog({ categories, productsByCat, heroImageUrl
     const scrollT = setTimeout(() => {
       const el = document.getElementById(`product-${f}`);
       if (el) el.scrollIntoView({ behavior: "smooth", block: "center" });
+      const params = new URLSearchParams(window.location.search);
+      params.delete("focus");
+      router.replace(`${pathname}?${params.toString()}`, { scroll: false });
     }, 150);
     const clearT = setTimeout(() => setFocusId(null), 2200);
-    const params = new URLSearchParams(searchParams.toString());
-    params.delete("focus");
-    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
     return () => { clearTimeout(scrollT); clearTimeout(clearT); };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams]);
