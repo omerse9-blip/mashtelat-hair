3ecc98e855d9a55ae2a93fe83ca9a48b114426de
omerse9-blip <omerse9@gmail.com>
2026-09-02 21:23:44 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [360, 420, 640, 768, 1024, 1200],
     imageSizes: [80, 120, 160, 240, 320, 480],
+    unoptimized: true,
   },
 };
 export default nextConfig;
